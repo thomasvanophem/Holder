4c4f92dcab9b3c5e3bfdddd9edd487938f7b5de5
Thomas van Ophem <thomas.vanophem@gmail.com>
2016-10-16 14:51:58 +0200

    Added credit card number validation using Luhn Algorithm and regexes to determine card type

--- Exercise 1/js/validate.js
+++ Exercise 1/js/validate.js
@@ -9,12 +9,71 @@ function validateCardName(name) {
 	}
 }
 
+/*
+    Get the card type
+    - Visa (0): 
+        - number starts with 4
+        - length: 13, 16 or 19
+    - Mastercard (1):
+        - number starts with 2221-2720 or 51-55
+        - length: 16
+    - American Express (2):
+        - number starts with34 or 37
+        - length: 15
+    - Other (3):
+        - length: 12 (maestro) to 19
+*/
 function getCardType(number) {
+    var temp = number.toString().replace(/[^\d]/g, "");
 
+    if (/^4[0-9]{12}(?:[0-9]{3})?(?:[0-9]{3})?$/.test(temp)) {
+        return 0;
+    } else if (/^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/.test(temp)) {
+        return 1;
+    } else if (/^3[47][0-9]{13}$/.test(temp)) {
+        return 2;
+    } else {
+        return 3;
+    }
 }
 
-function validateCardNumber(number) {
+/*
+    Validate the card number using the Luhn Algorithm
 
+    Luhn Algorithm:
+        - Drop last digit (check digit)
+        - Reverse numbers
+        - Multiply numbers in odd positions by 2
+            - substract 9 if result is higher than 9
+        - Add all numbers (even positions and results from previous step) together
+        - Add check digit
+        - If the result % 10 equals 0 the number is valid
+*/
+function validateCardNumber(number) {
+    var result = 0;
+
+    // Remove all characters but numbers
+    var temp = number.toString().replace(/[^\d]/g, "");
+
+    if (temp.length == 0) {
+        return false;
+    }
+
+    for (var i = temp.length - 1; i >= 0; i--) {
+        var number = parseInt(temp.charAt(i));
+
+        if (i % 2 == 0) {
+            number *= 2;
+
+            if (number > 9) {
+                number -= 9
+            }
+        }
+
+        result += number;
+    }
+
+    return (result % 10 == 0);
 }
 
 /*
@@ -61,5 +120,17 @@ $(document).ready(function() {
     	} else {
             invalid(parent);
     	}
+    });
+
+    $("#card-number").bind("change focusout", function() {
+        var parent = $(this).parent();
+
+        if (validateCardNumber($(this).val())) {
+            valid(parent);
+        } else {
+            invalid(parent);
+        }
+
+        $("#card-type").val(getCardType($(this).val()));
     });
 });
